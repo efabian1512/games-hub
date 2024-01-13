6cfe7f39df9f9f213b851e7120099088c71f1ce0
efabian1512 <efabian1989@hotmail.com>
2024-01-12 22:23:28 -0400

    show a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,16 @@
 import useGenres from "../hooks/useGenres";
-import { Image, List, ListItem } from '@chakra-ui/react';
+import { Image, List, ListItem, Spinner } from '@chakra-ui/react';
 import { HStack, Text } from '@chakra-ui/react';
 import getCroppedImageUrl from "../services/image.url";
 
 
 const GenreList = () => {
 
-    const { data } = useGenres();
+    const { data, isLoading, error } = useGenres();
+    
+    if (error) return null;
+    if (isLoading) return <Spinner/>;
+
     return (
         <List>
             {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
