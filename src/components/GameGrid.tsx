001225e6ad9025959992695a0bc4564e488d3829
efabian1512 <efabian1989@hotmail.com>
2024-01-12 21:42:07 -0400

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid = () => {
     return (
         <>
          {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm:1, md:2, lg:3, xl: 5}} spacing={10} padding='10px'>
+        <SimpleGrid columns={{sm:1, md:2, lg:3, xl: 5}} spacingX={3} spacingY={10} padding='10px'>
             {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
             {data.map(game => <GameCardContainer><GameCard key={game.id} game={game} /></GameCardContainer>)}
         </SimpleGrid>
