fe4826bd01c0c7aae65ee2cdc26d7e6738f4a283
efabian1512 <efabian1989@hotmail.com>
2024-01-23 19:28:15 -0400

    Fixing image resolution issue

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,20 @@
-import { HStack, Image,Box } from "@chakra-ui/react";
+import { HStack, Image,Box, useOutsideClick } from "@chakra-ui/react";
 import logo from '../assets/logo.webp';
 import useGameQueryStore from "../store";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link, useNavigate } from 'react-router-dom';
+import { useRef } from "react";
 
 
 
 const NavBar = () => {
+    const ref = useRef<HTMLImageElement>(null);
     const navigate = useNavigate();
-   
+
     return(
        <HStack padding='10px'>
-           <Link to='/'>
-               <Image src={logo} boxSize="60px" objectFit='cover'/>
-            </Link>
+           <img width='60px' height='60px' style={{cursor: 'pointer'}} src={logo} onClick={()=> navigate('/')}/>
            <SearchInput/>
            <ColorModeSwitch/>
        </HStack> 
